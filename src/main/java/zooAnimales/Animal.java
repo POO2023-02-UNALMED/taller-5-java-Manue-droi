a830e9b43faaacd7d6c3246c7c4691b45a167369
Manue-droi <manusarf16@gmail.com>
2023-09-26 08:21:52 -0500

    jsjsj

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -1,18 +1,62 @@
 package zooAnimales;
+import gestion.Zona;
 
 public class Animal {
-	 int totalAnimales; 
-	 String nombre;
-	 int edad;
-	 String habitat;
-	 String genero;
-	 zona array[];
-	 
-	 public Animal(String nombre, int totalAnimales, int edad, String habitat, String genero){
+    private static int totalAnimales;
+    private String nombre;
+    private int edad;
+    private String habitat;
+    private String genero;
+    private Zona zona;
 
-	 }
+    public Animal(String nombre, int edad, String habitat, String genero) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.habitat = habitat;
+        this.genero = genero;
+        totalAnimales ++;
+    }
+
+    public Animal() { totalAnimales ++;}
+
+    public String movimiento() {
+        return "desplazarse";
+    }
+
+    public static String totalPorTipo() {
+        return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" +
+                "Aves: " + Ave.cantidadAves() + "\n" +
+                "Reptiles: " + Reptil.cantidadReptiles() + "\n" +
+                "Peces: " + Pez.cantidadPeces() + "\n" +
+                "Anfibios: " + Anfibio.cantidadAnfibios();
+    }
+
+    @Override
+    public String toString() {
+        if(zona == null) {
+            return String.format("Mi nombre es %s, tengo una edad de %d, habito en %s y mi genero es %s", nombre, edad, habitat, genero);
+        } else {
+            return String.format("Mi nombre es %s, tengo una edad de %d, habito en %s y mi genero es %s, la zona en la que me ubico es %s, en el %s", nombre, edad, habitat, genero, zona, zona.getZoo().getNombre());
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
 }
-		 
 	
 	
 
