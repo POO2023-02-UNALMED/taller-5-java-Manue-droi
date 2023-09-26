09d950c0d988c415493954edd1e88ca65471ec25
Manue-droi <manusarf16@gmail.com>
2023-09-26 08:16:58 -0500

    kdkdk

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,14 +1,56 @@
 package gestion;
 
-public  class Zoologico {
-	String nombre;
-	String ubicación;
-	Zona array;
+import java.util.ArrayList;
+
+public class Zoologico {
+
+	private String nombre;
+	private String ubicacion;
+	private ArrayList<Zona> zonas = new ArrayList<>();
 	
-	public agregarZonas();
-	
-	public cantidadToltalAnimales();
-	
-	
-	
+    public Zoologico() {
+        this(null, null);
+    }
+
+    public Zoologico(String nombre, String ubicacion) {
+        this.nombre = nombre;
+        this.ubicacion = ubicacion;
+    }
+
+    public void agregarZonas(Zona zona) {
+        this.zonas.add(zona);
+        zona.setZoo(this);
+    }
+
+    public int cantidadTotalAnimales() {
+        int contador = 0;
+        for (int i=0; i< zonas.size();i++) {
+        	contador += this.zonas.get(i).cantidadAnimales();
+        }
+        return contador;		
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public ArrayList<Zona> getZona() {
+        return zonas;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public void setZona(ArrayList<Zona> zonas) {
+        this.zonas = zonas;
+    }
 }
